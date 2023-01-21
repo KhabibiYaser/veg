72fde3691b821215095a9ac0936596fc6be6049f
KhabibiYaser <skrillex299@mail.ru>
2023-01-21 18:49:41 +0300

    Initial commit

---
+++ finessa/src/Joji.java
@@ -0,0 +1,14 @@
+public class Joji {
+
+    int a = 324;
+    int b = 12;
+
+
+    public int getB() {
+        return b;
+    }
+
+    public int getA() {
+        return a;
+    }
+}
---
+++ finessa/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+        Joji a = new Joji();
+
+        System.out.println(a.a = a.b);
+
+
+
+    }
+
+
+
+
+
+
+}
